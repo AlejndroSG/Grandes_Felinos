14dedad3248044e1e073f694ece5ee833f6eb3c4
AlejndroSG <alex2005sg@gmail.com>
2025-01-21 18:10:41 +0100

    2025-01-21

--- equipo.php
+++ equipo.php
@@ -7,83 +7,81 @@
 <body id="team">
     <?php require_once('header.html'); ?>
     <main>
-        <!-- Conoce a los Miembros del Equipo -->
-        <section id="miembros-equipo" class="bg-light py-5 container">
+        <section id="miembros-equipo" class=" py-5 container">
             <h1 class="text-center mb-5">Nuestro Equipo</h1>
             <div class="row g-4">
-                <div class="col-md-4 text-center">
+                <div class="col-md-6 my-3 text-center text-white border border-white rounded py-5">
                     <img src="./fotos/equipo/1.jpg" class="rounded-circle mb-3" alt="Miembro 1">
                     <h5>Rachid Piñero</h5>
-                    <p class="text-muted">Diseñador Web</p>
+                    <p class="text-danger">Diseñador Web</p>
                     <p>"Apasionado por crear experiencias visuales únicas."</p>
                 </div>
-                <div class="col-md-4 text-center">
+                <div class="col-md-6 my-3 text-center text-white border border-white rounded py-5">
                     <img src="./fotos/equipo/2.jpg" class="rounded-circle mb-3" alt="Miembro 2">
                     <h5>Maria Pino</h5>
-                    <p class="text-muted">Desarrolladora</p>
+                    <p class="text-danger">Desarrolladora</p>
                     <p>"Convirtiendo ideas en soluciones funcionales."</p>
                 </div>
-                <div class="col-md-4 text-center">
+                <div class="col-md-6 my-3 text-center text-white border border-white rounded py-5">
                     <img src="./fotos/equipo/3.jpg" class="rounded-circle mb-3" alt="Miembro 3">
                     <h5>Cesar Olivera</h5>
-                    <p class="text-muted">Gerente de Proyecto</p>
+                    <p class="text-danger">Gerente de Proyecto</p>
                     <p>"Liderando proyectos con pasión y eficacia."</p>
                 </div>
-                <div class="col-md-4 text-center">
-                    <img src="./fotos/equipo/4.jpg" class="rounded-circle mb-3" alt="Miembro 1">
+                <div class="col-md-6 my-3 text-center text-white border border-white rounded py-5">
+                    <img src="./fotos/equipo/4.jpg" class="rounded-circle mb-3" alt="Miembro 4">
                     <h5>Anais Verdu</h5>
-                    <p class="text-muted">Artista Digital</p>
+                    <p class="text-danger">Artista Digital</p>
                     <p>"Convirtiendo ideas en visuales impactantes."</p>
                 </div>
-                <div class="col-md-4 text-center">
-                    <img src="./fotos/equipo/5.jpg" class="rounded-circle mb-3" alt="Miembro 2">
+                <div class="col-md-6 my-3 text-center text-white border border-white rounded py-5">
+                    <img src="./fotos/equipo/5.jpg" class="rounded-circle mb-3" alt="Miembro 5">
                     <h5>Lorenzo Castañeda</h5>
-                    <p class="text-muted">Fotógrafo</p>
+                    <p class="text-danger">Fotógrafo</p>
                     <p>"Capturando momentos extraordinarios."</p>
                 </div>
-                <div class="col-md-4 text-center">
-                    <img src="./fotos/equipo/6.jpg" class="rounded-circle mb-3" alt="Miembro 3">
+                <div class="col-md-6 my-3 text-center text-white border border-white rounded py-5">
+                    <img src="./fotos/equipo/6.jpg" class="rounded-circle mb-3" alt="Miembro 6">
                     <h5>Cristian San-Jose</h5>
-                    <p class="text-muted">Coordinador de equipo de grabación</p>
+                    <p class="text-danger">Coordinador de equipo de grabación</p>
                     <p>"Trabajando en equipo para crear contenido impactante."</p>
                 </div>
             </div>
         </section>
 
-        <!-- Nuestro Proceso de Trabajo -->
-        <section id="proceso-trabajo" class="container bg-light py-5 my-0">
+        <section id="proceso-trabajo" class="container  py-5 my-0">
             <div class="row">
                 <div class="col-md-12 text-center">
                     <h2 class="mb-4">Nuestro Proceso</h2>
-                    <p class="fs-5">Desde la idea inicial hasta el resultado final, trabajamos de manera colaborativa y estratégica para alcanzar nuestros objetivos.</p>
+                    <p class="fs-5 text-white">Desde la idea inicial hasta el resultado final, trabajamos de manera colaborativa y estratégica para alcanzar nuestros objetivos.</p>
                     <div class="row g-4 text-center mt-4">
                         <div class="col-md-9 contenido">
-                            <div class="card p-3 h-100 p-5">
-                                <h5>Investigación y Análisis</h5>
+                            <div class="card text-light p-3 p-md-5">
+                                <h5 class="text-danger fs-3">Investigación y Análisis</h5>
                                 <p>Comenzamos con una investigación exhaustiva sobre el estado actual de las especies de grandes felinos, sus hábitats y las amenazas que enfrentan. Analizamos datos científicos y colaboramos con expertos para tomar decisiones informadas.</p>
                                 <img src="./fotos/equipo/investigacion.jpg" alt="">
                             </div>
-                            <div class="card p-3 h-100 p-5">
-                                <h5>Planificación Estratégica</h5>
+                            <div class="card text-light p-3 p-md-5">
+                                <h5 class="text-danger fs-3">Planificación Estratégica</h5>
                                 <p>Diseñamos iniciativas prácticas y escalables para abordar los problemas identificados. Esto incluye estrategias de conservación, sensibilización pública y colaboración con comunidades locales.</p>
                                 <img src="./fotos/equipo/estrategia.png" alt="">
                             </div>
-                            <div class="card p-3 h-100 p-5">
-                                <h5>Implementación en Terreno</h5>
+                            <div class="card text-light p-3 p-md-5">
+                                <h5 class="text-danger fs-3">Implementación en Terreno</h5>
                                 <p>Nuestro equipo ejecuta las estrategias directamente en los ecosistemas de los grandes felinos. Esto incluye actividades como reforestación, creación de corredores ecológicos y rescate de animales en peligro.</p>
                                 <img src="./fotos/equipo/terreno.jpg" alt="">
                             </div>
-                            <div class="card p-3 h-100 p-5">
-                                <h5>Monitoreo y Divulgación</h5>
+                            <div class="card text-light p-3 p-md-5">
+                                <h5 class="text-danger fs-3">Monitoreo y Divulgación</h5>
                                 <p>Evaluamos el impacto de nuestras acciones mediante monitoreo constante y compartimos los resultados con la comunidad global. Informar a los colaboradores y al público asegura transparencia y refuerza el apoyo.</p>
                                 <img src="./fotos/equipo/monitoreo.jpg" alt="">
                             </div>
                         </div>
                         <div class="col-md-3 botones">
-                            <button class="active text-white my-4" >1</button>
-                            <button class="active text-white my-4">2</button>
-                            <button class="active text-white my-4">3</button>
-                            <button class="active text-white my-4">4</button>
+                            <button class="active text-white my-4 rounded-5 p-3">1</button>
+                            <button class="active text-white my-4 rounded-5 p-3">2</button>
+                            <button class="active text-white my-4 rounded-5 p-3">3</button>
+                            <button class="active text-white my-4 rounded-5 p-3">4</button>
                         </div>
                     </div>
                 </div>
@@ -91,7 +89,7 @@
         </section>
 
         <!-- Logros y Reconocimientos -->
-        <section id="logros-reconocimientos" class="bg-light py-5 container my-0">
+        <section id="logros-reconocimientos" class=" py-5 container my-0">
             <h2 class="text-center mb-4">Nuestros Logros</h2>
             <div class="row text-center g-4">
                 <div class="col-md-4">
@@ -115,7 +113,7 @@
             </div>
         </section>
 
-        <section class="container-fluid bg-light py-5" id="unete-iniciativas">
+        <section class="container-fluid  py-5" id="unete-iniciativas">
             <div class="row justify-content-center text-center">
                 <div class="col-md-8">
                     <h2 class="mb-4">Únete a nuestras iniciativas</h2>
--- index.php
+++ index.php
@@ -7,7 +7,8 @@
 <body id="principal">
     <?php require_once('header.html'); ?>
     <main class="oscuridad">
-        <section class="container-fluid">
+        <section class="container-fluid position-relative">
+            <video src="./fotos/Inicio/Grandes_Felinos.mp4" loop muted autoplay poster="./fotos/Inicio/img1.jpg"></video>
             <div class="row">
                 <div class="col text-center py-5 text-white">
                     <h1>Grandes Felinos</h1>
@@ -17,42 +18,34 @@
             </div>
         </section>
         <section class="container-fluid">
-            <div class="row dates">
-                <div class="col text-center text-white">
-                    <div class="p-5">
-                        <div class="container-fluid">
-                            <div class="row justify-content-center">
-                                <div class="col-12 col-md-3 text-center">
-                                    <div class="mb-3">
-                                        <img src="./fotos/Inicio/Rodaje_Documental.jpg" alt="Equipo de cineastas en acción" class="img-fluid rounded shadow">
-                                    </div>
-                                    <h3>Inicio del Proyecto</h3>
-                                    <p>En 2020, un equipo de cineastas apasionados comenzó a explorar la vida de los grandes felinos.</p>
-                                </div>
-                                <div class="col-12 col-md-3 text-center">
-                                    <div class="mb-3">
-                                        <img src="./fotos/Inicio/img5.jpg" alt="Felino en su hábitat natural" class="img-fluid rounded shadow">
-                                    </div>
-                                    <h3>Exploración de Hábitats</h3>
-                                    <p>Se filmaron escenas en selvas, sabanas y montañas para capturar la esencia de estos depredadores.</p>
-                                </div>
-                                <div class="col-12 col-md-3 text-center">
-                                    <div class="mb-3">
-                                        <img src="./fotos/Inicio/img6.jpg" alt="Felinos en peligro de extinción" class="img-fluid rounded shadow">
-                                    </div>
-                                    <h3>Mensaje Central</h3>
-                                    <p>La importancia de conservar el equilibrio natural y proteger a estas especies en peligro.</p>
-                                </div>
-                                <div class="col-12 col-md-3 text-center">
-                                    <div class="mb-3">
-                                        <img src="./fotos/Inicio/img7.jpg" alt="Personas ayudando a la conservación" class="img-fluid rounded shadow">
-                                    </div>
-                                    <h3>Impacto Global</h3>
-                                    <p>El documental ha inspirado a miles a unirse a iniciativas de conservación en todo el mundo.</p>
-                                </div>
-                            </div>
-                        </div>
+            <div class="row dates py-5 px-3 text-white">
+                <div class="col-12 col-lg-3 text-center">
+                    <div class="mb-3">
+                        <img src="./fotos/Inicio/Rodaje_Documental.jpg" alt="Equipo de cineastas en acción" class="img-fluid rounded shadow">
                     </div>
+                    <h3>Inicio del Proyecto</h3>
+                    <p>En 2020, un equipo de cineastas apasionados comenzó a explorar la vida de los grandes felinos.</p>
+                </div>
+                <div class="col-12 col-lg-3 text-center">
+                    <div class="mb-3">
+                        <img src="./fotos/Inicio/img5.jpg" alt="Felino en su hábitat natural" class="img-fluid rounded shadow">
+                    </div>
+                    <h3>Exploración de Hábitats</h3>
+                    <p>Se filmaron escenas en selvas, sabanas y montañas para capturar la esencia de estos depredadores.</p>
+                </div>
+                <div class="col-12 col-lg-3 text-center">
+                    <div class="mb-3">
+                        <img src="./fotos/Inicio/img6.jpg" alt="Felinos en peligro de extinción" class="img-fluid rounded shadow">
+                    </div>
+                    <h3>Mensaje Central</h3>
+                    <p>La importancia de conservar el equilibrio natural y proteger a estas especies en peligro.</p>
+                </div>
+                <div class="col-12 col-lg-3 text-center">
+                    <div class="mb-3">
+                        <img src="./fotos/Inicio/img7.jpg" alt="Personas ayudando a la conservación" class="img-fluid rounded shadow">
+                    </div>
+                    <h3>Impacto Global</h3>
+                    <p>El documental ha inspirado a miles a unirse a iniciativas de conservación en todo el mundo.</p>
                 </div>
             </div>
         </section>
@@ -61,16 +54,16 @@
                 <div id="cta-section" class="cta py-5 text-center col-md-6">
                     <div class="container flex-wrap">
                         <h2 class="cta-title mb-3">Por qué importan los grandes felinos</h2>
-                        <p class="cta-text mb-4">
+                        <p class="fs-5 mb-4">
                         50% de los hábitats de grandes felinos se han perdido en el último siglo.
                         </p>
-                        <p class="cta-text mb-4">Menos del 7% de los leopardos persas sobreviven en estado salvaje.</p>
+                        <p class="fs-5 mb-4">Menos del 7% de los leopardos persas sobreviven en estado salvaje.</p>
                     </div>
                 </div>
                 <div id="cta-section" class="cta py-5 text-center col-md-6">
                     <div class="container">
                         <h2 class="cta-title mb-3">Descubre cómo ayudar</h2>
-                        <p class="cta-text mb-4">
+                        <p class="fs-5 mb-4">
                         Únete a nuestras iniciativas para proteger a los grandes felinos y su hábitat. Cada aporte cuenta para preservar su futuro.
                         </p>
                         <a href="#acciones" class="btn btn-primary btn-lg">Descubre cómo ayudar</a>
